1bee9702b7196ec7cce9c2c631b3e963ccb46ce7
Wilson <wilson.boca@gmail.com>
2020-05-15 17:59:11 -0300

    adding map

--- src/app.py
+++ src/app.py
@@ -82,8 +82,7 @@ us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/maste
 map = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
                         color_discrete_sequence=["fuchsia"], zoom=5, height=300)
 map.update_layout(mapbox_style="open-street-map")
-map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
+map.update_layout(margin={"r": 0,"t": 0,"l": 0,"b": 0})
 
 app.layout = html.Div([
     html.Div([
